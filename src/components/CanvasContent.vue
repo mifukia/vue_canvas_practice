9d80321c8f0b9f5f80df0212b1a1291d86840eec
Akifumi Shimoji <shimoji@3kaku.co.jp>
2019-07-05 21:58:25 +0900

    canvas first

--- src/components/CanvasContent.vue
+++ src/components/CanvasContent.vue
@@ -10,11 +10,51 @@ export default {
   mounted() {
     this.canvasInit();
   },
+  computed: {
+    context2D() {
+      return this.canvasElement.getContext("2d");
+    }
+  },
   methods: {
     canvasInit() {
-      const canvasElement = document.getElementById("myCanvas");
-      const context2D = canvasElement.getContext("2d");
-      context2D.fillRect(10, 10, 100, 100);
+      this.canvasElement = document.getElementById("myCanvas");
+      this.draw(this.context2D);
+    },
+    draw(context) {
+      context.beginPath();
+      context.strokeStyle = "#0000ff";
+      context.fillStyle = "#00ffff";
+      context.closePath();
+      // this.drawRect(context, 10, 10, 100, 100);
+      this.drawStar(context, 100, 100, 5, 80, 30);
+      context.fill();
+      context.stroke();
+    },
+    drawRect(context, nX, nY, nWidth, nHeight) {
+      context.moveTo(nX, nY);
+      context.lineTo(nX + nWidth, nY);
+      context.lineTo(nX + nWidth, nY + nHeight);
+      context.lineTo(nX, nY + nHeight);
+      context.lineTo(nX, nY);
+    },
+    drawStar(context, nX, nY, numVertex, longRadius, shortRadius) {
+      const nTheta = Math.PI / numVertex;
+      let nRadians = -Math.PI / 2;
+
+      context.moveTo(nX, nY - longRadius);
+
+      for (let i = 1; i <= numVertex; i++) {
+        nRadians += nTheta;
+        context.lineTo(
+          shortRadius * Math.cos(nRadians) + nX,
+          shortRadius * Math.sin(nRadians) + nY
+        );
+        nRadians += nTheta;
+        context.lineTo(
+          longRadius * Math.cos(nRadians) + nX,
+          longRadius * Math.sin(nRadians) + nY
+        );
+      }
     }
   }
 };
