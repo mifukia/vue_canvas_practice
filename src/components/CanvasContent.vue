25e8f11b31f2c8dbf0d49d7c6220a32fce839761
Akifumi Shimoji <shimoji@3kaku.co.jp>
2019-07-05 18:32:02 +0900

    init

---
+++ src/components/CanvasContent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="canvasArea">
+    <canvas id="myCanvas" width="240" height="180"></canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CanvasContent",
+  mounted() {
+    this.canvasInit();
+  },
+  methods: {
+    canvasInit() {
+      const canvasElement = document.getElementById("myCanvas");
+      const context2D = canvasElement.getContext("2d");
+      context2D.fillRect(10, 10, 100, 100);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.canvasArea {
+  margin-top: 100px;
+}
+canvas {
+  border: 1px solid #333;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <canvas-content />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CanvasContent from "@/components/CanvasContent.vue";
+
+export default {
+  name: "home",
+  components: {
+    CanvasContent
+  }
+};
+</script>
